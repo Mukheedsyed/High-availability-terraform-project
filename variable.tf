74c91d23a509fb45987cfa8ae058287ce343eef1
syed mukheed <syedmukheed777@gmail.com>
2023-01-25 12:16:30 +0400

    first code

---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "vpc-cidr" {
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "public-subnet-1-cidr" {
+  default     = "10.0.0.0/24"
+  description = "public subnet 1 CIDR Block"
+  type        = string
+}
+
+variable "public-subnet-2-cidr" {
+  default     = "10.0.1.0/24"
+  description = "public subnet 2 CIDR Block"
+  type        = string
+}
+
+variable "private-subnet-1-cidr" {
+  default     = "10.0.2.0/24"
+  description = "private subnet 1 CIDR Block"
+  type        = string
+}
+
+variable "private-subnet-2-cidr" {
+  default     = "10.0.3.0/24"
+  description = "private subnet 2 CIDR Block"
+  type        = string
+}
+
+variable "ssh-location" {
+  default     = "0.0.0.0/0"
+  description = "IP Address that can SSH in to EC2 Intance"
+  type        = string
+}
