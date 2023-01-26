2cf924aad6255cf165a3dd85c212ccc3e473f6d9
syed mukheed <syedmukheed777@gmail.com>
2023-01-26 14:22:36 +0400

    sg code

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# vpc module
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -6,7 +7,7 @@ module "vpc" {
 
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
@@ -15,4 +16,34 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+# security group module
+module "elb_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "my-elb"
+  description = "Enable HTTP/HTTPS access on port 80/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
+  ingress_rules            = ["http-80-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "HTTP Access"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "HTTPS Access"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    
+  ]
 }
